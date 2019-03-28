91b48981f97d0042152d7ad2b996b38f5b1b2415
ericafenyo <ericafenyo@yahoo.com>
2019-03-28 03:19:34 +0100

    code clean up, generate css from sass file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getForecasts } from './javascript/data/Repository'
+import React, { Component } from "react";
+import "./App.css";
+import { getForecasts } from "./javascript/data/Repository";
 
 const WeatherCard = ({ day, forecast }) => {
-  console.log(forecast.Day.Icon)
-  const baseStyle = (i) => {
+  console.log(forecast.Day.Icon);
+  const baseStyle = i => {
     return {
       backgroundImage: `url(https://vortex.accuweather.com/adc2010/images/slate/icons/${i}.svg)`
-    }
-  }
+    };
+  };
 
   return (
     <div className="card">
       <p className="over-line color-caption">{day}</p>
-      <div className="icon" style={baseStyle(forecast.Day.Icon)}></div>
+      <div className="icon" style={baseStyle(forecast.Day.Icon)} />
       <div className="card-body">
         <div className="">
-          <div className="d-flex">
-            <p className="caption">{forecast ? forecast.Temperature.Maximum.Value + " °" : ""}</p>
-            <p className="caption color-caption">{forecast ? forecast.Temperature.Minimum.Value + " °" : ""}</p>
+          <div style={{ display: "flex" }}>
+            <p className="caption mx-4px ">
+              {forecast ? forecast.Temperature.Maximum.Value + " °" : ""}
+            </p>
+            <p className="caption mx-4px  color-caption">
+              {forecast ? forecast.Temperature.Minimum.Value + " °" : ""}
+            </p>
           </div>
         </div>
       </div>
-    </div>)
-}
-
+    </div>
+  );
+};
 
 class App extends Component {
   constructor(props) {
@@ -33,62 +37,72 @@ class App extends Component {
       city: "",
       forecast: [],
       isLoading: true
-    }
+    };
   }
 
   loadData = () => {
     getForecasts((location, data) => {
-      this.setState({ isLoading: true })
+      console.log("location");
+      this.setState({ isLoading: true });
       this.setState({
         city: location,
         forecast: data
-      })
-      this.setState({ isLoading: false })
-    })
-  }
+      });
+      this.setState({ isLoading: false });
+    });
+  };
 
   componentDidMount() {
-    this.loadData()
+    this.loadData();
     setInterval(() => {
-      this.loadData()
-    }, 600000)
+      this.loadData();
+    }, 600000);
   }
 
-  parseDay = (date) => new Date(date).toDateString().split(" ").shift()
+  parseDay = date =>
+    new Date(date)
+      .toDateString()
+      .split(" ")
+      .shift();
 
-  next 
+  previous = () => {
+    console.log("previous");
+  };
+
+  next = () => {
+    console.log("next");
+  };
 
   render() {
-    let forecasts = this.state.forecast
-    console.log(this.state.isLoading)
+    let forecasts = this.state.forecast;
     return (
-      <div className="weather-board">
-
-        <div className="main">
-          <div className="stage"></div>
-
-          <div className="ok d-flex align-items-center justify-content-center">
-            <div className="fav-button">
-              <i class="fas fa-xs fa-chevron-left"></i>
-            </div>
-            <div className="carousel-wrapper">
-              <div className="carousel container">
-                {
-                  this.state.isLoading ?
-                    <p>Loading...</p> :
-                    forecasts.map((forecast, index) => {
-                      console.log(forecast)
-                      return <WeatherCard forecast={forecast} day={this.parseDay(forecast.Date)} key={index} />
-                    })
-                }
-              </div>
-            </div>
-
-            <div className="fav-button" onClick = {this.next}>
-              <i class="fas fa-xs fa-chevron-right"></i>
+      <div className="parent-card">
+        <div className="stage" />
+        <div className="ok d-flex align-items-center justify-content-center">
+          <div className="fav-button" onClick={this.previous}>
+            <i className="fas fa-xs fa-chevron-left" />
+          </div>
+          <div className="carousel-wrapper">
+            <div className="carousel container">
+              {this.state.isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                forecasts.map((forecast, index) => {
+                  console.log(forecast);
+                  return (
+                    <WeatherCard
+                      forecast={forecast}
+                      day={this.parseDay(forecast.Date)}
+                      key={index}
+                    />
+                  );
+                })
+              )}
             </div>
           </div>
-
+          <div className="fav-button" onClick={this.next}>
+            <i className="fas fa-xs fa-chevron-right pl-2px " />
+          </div>
         </div>
       </div>
     );
